966ea6fb6d0a2c04ecda7f8bbb8a4cbd682ca5f0
Guillermo Moreno <contact@memobrown.com>
2021-03-27 17:19:55 -0600

    Hero.vue finished

---
+++ components/Hero.vue
@@ -0,0 +1,74 @@
+<template>
+  <main class="hero">
+    <VueSlickCarousel
+      :arrows="false"
+      :dots="true"
+      :infinite="true"
+      :autoplay="true"
+      :fade="true"
+    >
+      <div class="carousel-item">
+        <div class="title-container">
+          <h1 class="title">
+            Muestra al mundo la<br /><span>anatomía</span><br />de tu marca
+          </h1>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="title-container">
+          <h1 class="title">
+            Muestra al mundo la<br /><span>anatomía</span><br />de tu marca
+          </h1>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="title-container">
+          <h1 class="title">
+            Muestra al mundo la<br /><span>anatomía</span><br />de tu marca
+          </h1>
+        </div>
+      </div>
+    </VueSlickCarousel>
+  </main>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: 'MyComponent',
+  components: { VueSlickCarousel },
+}
+</script>
+
+<style scoped>
+/* .bg-hero img {
+  position: relative;
+  z-index: 0;
+  width: 100%;
+} */
+.carousel-item {
+  background-image: url('@/assets/images/hero.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  font-family: sifonn;
+}
+.title {
+  font-size: 3.4rem;
+  color: #fff;
+  margin-left: 20rem;
+  line-height: 8rem;
+}
+.title-container {
+  position: relative;
+  top: 30%;
+}
+.title span {
+  font-size: 16rem;
+  text-transform: uppercase;
+}
+</style>
--- pages/mexico/index.vue
+++ pages/mexico/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Navigation />
+    <Hero />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation'
+import Hero from '@/components/Hero'
 export default {
   components: {
     Navigation,
+    Hero,
   },
 }
 </script>
