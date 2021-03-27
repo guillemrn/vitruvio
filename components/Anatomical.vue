7e286f2d1f5137b206185f9523d62be2b7493071
Guillermo Moreno <contact@memobrown.com>
2021-03-27 17:41:35 -0600

    Anatomical.vue finished

---
+++ components/Anatomical.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <figure>
+      <img src="@/assets/images/man.svg" alt="man" />
+    </figure>
+    <div>
+      <h2>Bajo el concepto de<br /><span>Anatomical marketing</span></h2>
+      <p>Trabajamos bajo las reglas estéticas de la experiencia humana.</p>
+      <p>
+        Cuando juzgamos que algo es
+        <strong>BELLO, FEO, SUBLIME O ELEGANTE,</strong> estamos haciendo
+        haciendo juicios estéticos, basadas en la
+        <strong>anatomía física y conceptual</strong>
+        de las cosas.
+      </p>
+      <p>
+        Analizar y definir la <strong>ANATOMÍA DE TU MARCA,</strong> nos permite
+        ser realmente efectivos al momento de querer mostrarla.
+        <strong>Observarla, medirla y diseccionarla,</strong> hace posible
+        generar poderosos conceptos de comunicación, que atraigan la atención e
+        interés del cliente, orientando su decisión de compra a favor de tu
+        servicio o producto.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background-color: var(--yellow);
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+}
+h2 {
+  font-family: sifonn;
+  font-size: 2.8rem;
+  text-transform: uppercase;
+  text-align: center;
+  letter-spacing: 15px;
+}
+span {
+  font-size: 5.3rem;
+  letter-spacing: -1px;
+}
+p {
+  font-family: 'Century gothic';
+  font-size: 2.6rem;
+  margin: 4rem 0;
+  padding: 0 14rem;
+  letter-spacing: 1px;
+}
+</style>
--- pages/mexico/index.vue
+++ pages/mexico/index.vue
@@ -2,16 +2,19 @@
   <div>
     <Navigation />
     <Hero />
+    <Anatomical />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
+import Anatomical from '@/components/Anatomical'
 export default {
   components: {
     Navigation,
     Hero,
+    Anatomical,
   },
 }
 </script>
