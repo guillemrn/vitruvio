4e906623f7f7858d29fed77a079fe83937ea21fa
Guillermo Moreno <contact@memobrown.com>
2021-04-13 22:58:11 -0500

    Hero.vue responsive design

--- components/Hero.vue
+++ components/Hero.vue
@@ -72,4 +72,90 @@ export default {
   text-transform: uppercase;
   line-height: 130px;
 }
+
+/* -------------------------------------------Responsive design */
+
+@media screen and (max-width: 1536px) {
+  .carousel-item {
+    background-size: cover;
+  }
+  .title {
+    margin-left: 10rem;
+  }
+  .title span {
+    font-size: 12rem;
+  }
+}
+@media screen and (max-width: 1440px) {
+}
+@media screen and (max-width: 1280px) {
+  .title {
+    font-size: 3.5rem;
+  }
+  .title span {
+    font-size: 11rem;
+  }
+}
+@media screen and (max-width: 1024px) {
+}
+@media screen and (max-width: 768px) {
+  .carousel-item {
+    background-image: url('@/assets/images/hero-mobile.jpg');
+  }
+  .title-container {
+    position: initial;
+    margin-top: 20rem;
+  }
+  .title {
+    margin-left: 0;
+    text-align: center;
+  }
+  .title span {
+    line-height: 110px;
+  }
+}
+@media screen and (max-width: 576px) {
+  .title {
+    font-size: 2.5rem;
+  }
+  .title span {
+    line-height: 80px;
+    font-size: 9rem;
+  }
+  .title-container {
+    margin-top: 30rem;
+  }
+}
+@media screen and (max-width: 540px) {
+  .title span {
+    line-height: 70px;
+    font-size: 8rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+}
+@media screen and (max-width: 480px) {
+  .title span {
+    line-height: 60px;
+    font-size: 7rem;
+  }
+}
+@media screen and (max-width: 426px) {
+}
+@media screen and (max-width: 376px) {
+  .title span {
+    line-height: 50px;
+    font-size: 6rem;
+  }
+  .title {
+    font-size: 1.7rem;
+  }
+}
+@media screen and (max-width: 320px) {
+  .title span {
+    line-height: 40px;
+    font-size: 5rem;
+  }
+}
 </style>
--- pages/españa/index.vue
+++ pages/españa/index.vue
@@ -2,34 +2,34 @@
   <div>
     <Navigation />
     <Hero />
-    <Anatomical />
+    <!-- <Anatomical />
     <VideoSpain />
     <Services />
     <Team />
     <Metodologia />
-    <Footer />
+    <Footer /> -->
   </div>
 </template>
 
 <script>
-import Navigation from '@/components/Navigation'
-import Hero from '@/components/Hero'
-import Anatomical from '@/components/Anatomical'
-import VideoSpain from '@/components/VideoSpain'
-import Services from '@/components/Services'
-import Team from '@/components/Team'
-import Metodologia from '@/components/Metodologia'
-import Footer from '@/components/Footer'
-export default {
-  components: {
-    Navigation,
-    Hero,
-    Anatomical,
-    VideoSpain,
-    Services,
-    Team,
-    Metodologia,
-    Footer,
-  },
-}
+// import Navigation from '@/components/Navigation'
+// import Hero from '@/components/Hero'
+// import Anatomical from '@/components/Anatomical'
+// import VideoSpain from '@/components/VideoSpain'
+// import Services from '@/components/Services'
+// import Team from '@/components/Team'
+// import Metodologia from '@/components/Metodologia'
+// import Footer from '@/components/Footer'
+// export default {
+//   components: {
+//     Navigation,
+//     Hero,
+//     Anatomical,
+//     VideoSpain,
+//     Services,
+//     Team,
+//     Metodologia,
+//     Footer,
+//   },
+// }
 </script>
