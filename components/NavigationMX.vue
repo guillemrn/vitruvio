cd4e048311ff6684a3cf5771228ce02edcfb244d
Guillermo Moreno <contact@memobrown.com>
2021-04-16 12:32:22 -0500

    Changes

--- components/NavigationMX.vue
+++ components/NavigationMX.vue
@@ -132,7 +132,7 @@
   cursor: pointer;
   display: inline-block;
   float: right;
-  padding: 28px 20px;
+  padding: 40px 20px;
   position: relative;
   user-select: none;
 }
@@ -239,6 +239,10 @@
   }
 }
 @media screen and (max-width: 768px) {
+  .logo {
+    position: relative;
+    top: 12%;
+  }
 }
 @media screen and (max-width: 576px) {
   .logo img {
@@ -246,28 +250,13 @@
   }
 }
 @media screen and (max-width: 540px) {
-  .logo img {
-    width: 16rem;
-  }
 }
 @media screen and (max-width: 480px) {
-  .logo img {
-    width: 15rem;
-  }
 }
 @media screen and (max-width: 426px) {
-  .logo img {
-    width: 14rem;
-  }
 }
 @media screen and (max-width: 376px) {
-  .logo img {
-    width: 12rem;
-  }
 }
 @media screen and (max-width: 320px) {
-  .logo img {
-    width: 10rem;
-  }
 }
 </style>
