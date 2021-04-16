4198a1eead29f9ed5a8f51bd200366fdcef23c05
Guillermo Moreno <contact@memobrown.com>
2021-04-16 18:30:00 -0500

    up.vue fixed

--- components/Up.vue
+++ components/Up.vue
@@ -14,7 +14,7 @@
   z-index: 100;
 }
 .cta-whatsapp img {
-  width: 10rem;
+  width: 8rem;
   padding-right: 2rem;
   margin-left: 4rem;
 }
@@ -22,14 +22,44 @@ a {
   text-decoration: none;
 }
 
+/* -------------------------------------------Responsive design */
+
+@media screen and (max-width: 1536px) {
+  .cta-whatsapp img {
+    width: 7.5rem;
+  }
+}
+@media screen and (max-width: 1440px) {
+  .cta-whatsapp img {
+    width: 7rem;
+  }
+}
+@media screen and (max-width: 1280px) {
+  .cta-whatsapp img {
+    width: 6.5rem;
+  }
+}
+@media screen and (max-width: 1024px) {
+  .cta-whatsapp img {
+    width: 6rem;
+  }
+}
+
 @media screen and (max-width: 768px) {
   .cta-whatsapp img {
-    width: 8rem;
+    width: 6rem;
   }
 }
 @media screen and (max-width: 426px) {
   .cta-whatsapp img {
-    width: 7rem;
+    width: 5.5rem;
   }
+}
+@media screen and (max-width: 376px) {
+  .cta-whatsapp img {
+    width: 5rem;
+  }
+}
+@media screen and (max-width: 320px) {
 }
 </style>
